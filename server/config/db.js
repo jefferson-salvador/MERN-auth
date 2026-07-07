ebe93427b30d17d4c9092be20f4ae4118e1fb312
Jefferson Salvador <jeffsalvador.dev@gmail.com>
2026-07-07 08:02:48 +0800

    test: added log when connecting to MONGODB

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    console.log('Connecting to MONGODB...');
+    
     await mongoose.connect(process.env.MONGODB_URI);
 
     console.log('Connected to MONGODB');
