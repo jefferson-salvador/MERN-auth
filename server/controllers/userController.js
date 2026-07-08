5619a5307b80e9a5c5ab9ba09c82c3ea4fe04d45
Jefferson Salvador <jeffsalvador.dev@gmail.com>
2026-07-09 06:43:44 +0800

    feat: implemented login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const signUp = async (req, res) => {
   const { name, username, password } = req.body;
@@ -18,10 +19,12 @@ export const signUp = async (req, res) => {
       });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await User.create({
       name,
       username,
-      password,
+      password: hashedPassword,
     });
 
     return res.status(201).json({
@@ -32,6 +35,44 @@ export const signUp = async (req, res) => {
 
     return res.status(500).json({
       message: "Error creating user",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username and password are required",
+    });
+  }
+
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "Invalid username or password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid username or password",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Login successful",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Error logging in",
     });
   }
 };
